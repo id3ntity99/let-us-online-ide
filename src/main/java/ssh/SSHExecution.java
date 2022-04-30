37db259627da439f077bb1b8e7d41ffc008dc74e
id3ntity99 <jas06113@gmail.com>
2022-04-30 16:40:04 +0900

    refact: 에러처리 코드 변경 및 readInputStream 로직 변경

--- src/main/java/ssh/SSHExecution.java
+++ src/main/java/ssh/SSHExecution.java
@@ -2,18 +2,15 @@ package ssh;
 
 import com.jcraft.jsch.ChannelShell;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.function.IntToDoubleFunction;
+import java.util.Arrays;
 
 public class SSHExecution {
     private final ChannelShell channel;
     private InputStream inputStream;
     private OutputStream outputStream;
-    private static final System.Logger LOGGER = System.getLogger(SSHExecution.class.getName());
+    private byte[] byteBuffer = new byte[2000];
 
     /**
      * Constructor which takes ChannelShell as parameter.
@@ -22,14 +19,10 @@ public class SSHExecution {
      *
      * @param channel Existing channel that connects SSH client to SSH server.
      */
-    public SSHExecution(ChannelShell channel) {
+    public SSHExecution(ChannelShell channel) throws IOException {
         this.channel = channel;
-        try {
-            this.inputStream = channel.getInputStream();
-            this.outputStream = channel.getOutputStream();
-        } catch (IOException e) {
-            LOGGER.log(System.Logger.Level.ERROR, e);
-        }
+        this.inputStream = channel.getInputStream();
+        this.outputStream = channel.getOutputStream();
     }
 
     /**
@@ -39,29 +32,26 @@ public class SSHExecution {
      *
      * @param cmd Shell command to execute remotely.
      */
-    public void executeRemoteShell(String cmd) {
-        try {
-            outputStream.write(cmd.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
-        } catch (IOException e) {
-            LOGGER.log(System.Logger.Level.ERROR, e);
-        }
+    public void executeRemoteShell(String cmd) throws IOException {
+        outputStream.write(cmd.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
     }
 
 
     /**
      * Read the result of shell execution from the channel's input stream.
-     * The data from the input stream will then be stored in the byte array, whose size is 8192.
+     * The data from the input stream will then be stored in the byte array, whose size is 2000.
      * After writing into the byte array, it will be returned as a result of method calling.
      *
      * @return byte[], result of remote shell execution.
      */
-    private byte[] readInputStream() {
-        byte[] byteBuffer = new byte[2000];
-        try {
-            inputStream.read(byteBuffer, 0, byteBuffer.length);
-        } catch (IOException e) {
-            LOGGER.log(System.Logger.Level.ERROR, e);
+    private byte[] readInputStream() throws IOException {
+        Arrays.fill(byteBuffer, (byte) 0);
+        int readBytes = inputStream.read(byteBuffer, 0, byteBuffer.length);
+        if (readBytes != byteBuffer.length) { // if a number of bytes read from inputStream is less than byteBuffer's length
+            byte[] trimmedBuffer = new byte[readBytes];
+            System.arraycopy(byteBuffer, 0, trimmedBuffer, 0, readBytes);
+            byteBuffer = trimmedBuffer;
         }
         return byteBuffer;
     }
@@ -71,21 +61,21 @@ public class SSHExecution {
      * Listen to the channel's input stream.
      * If the input stream has bytes to be read, this method will
      * read data from it until there are nothing to read from the input stream.
+     * @return byte[], The last read 2000(or less) bytes.
      */
-    public void run() {
-        try {
-            Thread.sleep(50);
-            do {
-                byte[] byteOutput = readInputStream();
-                System.out.print(new String(byteOutput, StandardCharsets.UTF_8));
-                Thread.sleep(60);
-            }
-            while (channel.isConnected() && inputStream.available() > 0);
-        } catch (InterruptedException e) {
-            LOGGER.log(System.Logger.Level.ERROR, e);
-            Thread.currentThread().interrupt();
-        } catch (IOException e) {
-            LOGGER.log(System.Logger.Level.ERROR, e);
+    public byte[] run() throws InterruptedException, IOException {
+        if (!channel.isConnected()) {
+            throw new IOException("Channel is disconnected");
         }
+        if (inputStream.available() <= 0) {
+            throw new IOException("InputStream is empty");
+        }
+        Thread.sleep(100);
+        while (channel.isConnected() && inputStream.available() > 0) {
+            byte[] byteOutput = readInputStream();
+            System.out.print(new String(byteOutput, StandardCharsets.UTF_8));
+            Thread.sleep(60);
+        }
+        return byteBuffer;
     }
 }
