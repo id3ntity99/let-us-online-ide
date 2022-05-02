e214e1ed7820f5cbf7b04d30dcc49a63e586a5ec
id3ntity99 <jas06113@gmail.com>
2022-05-02 15:58:08 +0900

    chores: main 메서드

--- src/main/java/ssh/Main.java
+++ src/main/java/ssh/Main.java
@@ -1,6 +1,9 @@
 package ssh;
 
+import ssh.exceptions.InputTooLargeException;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     private static final System.Logger LOGGER = System.getLogger(Main.class.getName());
@@ -11,11 +14,21 @@ public class Main {
         final String pathToKnownHostsFile = "/home/yosef/.ssh/known_hosts";
         final String password = "1234";
         final int port = 22;
+        final byte[] cmd = "python3 test.py\n".getBytes(StandardCharsets.UTF_8);
+
         SSHConnection ssh = new SSHConnection();
         ssh.openNewSession(username, host, port, password, pathToKnownHostsFile);
         ssh.openChannel();
-        SSHExecution execution = new SSHExecution(ssh.getChannelShell());
-        execution.executeRemoteShell("python3 test.py\n");
-        execution.run();
+        try {
+            SSHExecution execution = new SSHExecution(ssh.getChannelShell());
+            execution.executeRemoteShell(cmd);
+            execution.run();
+        } catch (IOException | InputTooLargeException e) {
+            LOGGER.log(System.Logger.Level.ERROR, e);
+        } catch (InterruptedException e) {
+            LOGGER.log(System.Logger.Level.ERROR, e);
+            Thread.currentThread().interrupt();
+        }
+        ssh.closeConnection();
     }
 }
