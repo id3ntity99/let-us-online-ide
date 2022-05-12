ee29d07ccd66a399bdfd66ae31c533bab098c07a
id3ntity99 <jas06113@gmail.com>
2022-05-12 15:58:51 +0900

    refact: User 객체를 이용하여 SSH 통신

--- src/main/java/com/letus/ssh/SSHExecution.java
+++ src/main/java/com/letus/ssh/SSHExecution.java
@@ -2,7 +2,9 @@ package com.letus.ssh;
 
 import com.jcraft.jsch.ChannelShell;
 import com.letus.ssh.exceptions.InputTooLargeException;
+import com.letus.user.User;
 
+import javax.websocket.Session;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,8 +12,9 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class SSHExecution {
+    private SSHExecution() {}
     private static void writeCommand(OutputStream outputStream,
-                                    byte[] cmdBytes) throws IOException, InputTooLargeException {
+                                     byte[] cmdBytes) throws IOException, InputTooLargeException {
         if (cmdBytes.length > 2000) {
             throw new InputTooLargeException("Input size cannot exceed 2000 bytes");
         }
@@ -32,11 +35,15 @@ public class SSHExecution {
     }
 
 
-    public static byte[] readExecResult(byte[] cmd, ChannelShell channel, javax.websocket.Session clientSession)
+    public static byte[] readExecResult(byte[] cmd, User user)
             throws InterruptedException, IOException, InputTooLargeException {
+        ChannelShell channel = user.getChannel();
         InputStream inputStream = channel.getInputStream();
         OutputStream outputStream = channel.getOutputStream();
+        Session clientSession = user.getClientSession();
+
         writeCommand(outputStream, cmd);
+
         Thread.sleep(80);
         if (!channel.isConnected() && inputStream.available() <= 0) {
             throw new IOException("Either channel is not connected or input stream is empty");
