b70450ec0b41318b1e64a45944d83cdb5c19510c
id3ntity99 <jas06113@gmail.com>
2022-05-13 15:45:00 +0900

    feat: 유저 인증 및 매핑

--- src/main/java/com/letus/websocket/Websocket.java
+++ src/main/java/com/letus/websocket/Websocket.java
@@ -1,8 +1,9 @@
 package com.letus.websocket;
 
-import com.jcraft.jsch.JSch;
 import com.letus.ssh.SSHExecution;
+import com.letus.ssh.exceptions.InputTooLargeException;
 import com.letus.user.User;
+import com.letus.user.UserInfo;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -10,40 +11,59 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
-@ServerEndpoint("/init")
+@ServerEndpoint(value = "/init", configurator = HandShakeConfigurator.class)
 public class Websocket {
-    private final JSch jsch = new JSch();
-    private final String host = "172.17.0.2";
-    private final String username = "runner";
-    private final String pathToKnownHosts = "/home/yosef/.ssh/known_hosts";
-    private final String password = "1234";
-    private final int port = 22;
-    private static final HashMap<Session, User> sshChannelHashMap = new HashMap<>();
+    private static final String ID_TOKEN = "idToken";
+    private static final String HOST = "172.17.0.2";
+    private static final String USERNAME = "runner";
+    private static final String PATH_TO_KNOWN_HOSTS = "/home/yosef/.ssh/known_hosts";
+    private static final String PASSWORD = "1234";
+    private static final int PORT = 22;
+    private static final HashMap<String, User> sshChannelHashMap = new HashMap<>();
 
     @OnOpen
-    public void onOpen(Session session) throws Exception {
-        String clientSessionId = session.getId();
-        System.out.println("Total threads: " + Thread.activeCount());
-        if (!sshChannelHashMap.containsKey(clientSessionId)) {
+    public void onOpen(Session session) {
+        String idToken = (String) session.getUserProperties().get(ID_TOKEN);
+        if (!sshChannelHashMap.containsKey(idToken)) {
+            // UserInfo userInfo = Auth.getInfo(idToken);
+            UserInfo userInfo = new UserInfo("id", "email", true, "name", "url");
             session.setMaxIdleTimeout(1800000);
-            User user = new User(session);
-            user.openSSHConnection(username, host, port, password, pathToKnownHosts);
-            sshChannelHashMap.put(session, user);
+            try {
+                User user = new User(userInfo);
+                user.setSession(session);
+                user.openSSHConnection(USERNAME, HOST, PORT, PASSWORD, PATH_TO_KNOWN_HOSTS);
+                sshChannelHashMap.put(idToken, user);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            User user = sshChannelHashMap.get(idToken);
+            user.setSession(session);
+            System.out.println("The user already exists");
         }
     }
 
     @OnMessage
-    public void onMessage(Session session, String msg) throws Exception {
-        User user = sshChannelHashMap.get(session);
+    public void onMessage(Session session, String msg) {
+        String idToken = (String) session.getUserProperties().get(ID_TOKEN);
+        User user = sshChannelHashMap.get(idToken);
         byte[] cmd = msg.getBytes(StandardCharsets.UTF_8);
-        SSHExecution.readExecResult(cmd, user);
+        try {
+            SSHExecution.readExecResult(cmd, user);
+        } catch (IOException | InputTooLargeException e) {
+            e.printStackTrace();
+        }
     }
 
     @OnClose
-    public void onClose(Session session, CloseReason closeReason) throws IOException {
-        User user = sshChannelHashMap.get(session);
-        user.closeSSHConnection();
-        session.close();
-        System.out.println("Session closed");
+    public void onClose(Session session, CloseReason closeReason) {
+        String idToken = (String) session.getUserProperties().get(ID_TOKEN);
+        User user = sshChannelHashMap.get(idToken);
+        try {
+            user.getClientSession().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Websocket Session closed");
     }
 }
