e9ebd321ce0d1e9fec9ac546377455a1cfd1cdfe
id3ntity99 <jas06113@gmail.com>
2022-05-16 14:42:45 +0900

    refact: builder pattern applied

--- src/main/java/com/letus/user/User.java
+++ src/main/java/com/letus/user/User.java
@@ -1,38 +1,45 @@
 package com.letus.user;
 
-import com.google.gson.Gson;
+import com.github.dockerjava.api.model.Container;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.jcraft.jsch.ChannelShell;
-import com.jcraft.jsch.JSch;
-import com.letus.ssh.SSHConnection;
+import com.letus.tokenauth.Auth;
 
 import javax.websocket.Session;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class User {
     private ChannelShell channel;
     private Session clientSession;
-    private final SSHConnection conn;
-    private final UserInfo userInfo;
+    private Container container;
+    private GoogleIdToken.Payload payload;
 
-
-    public User(UserInfo userInfo) throws Exception {
-        this.conn = new SSHConnection(new JSch());
-        this.userInfo = userInfo;
+    public User(UserBuilder builder) {
+        this.channel = builder.channel;
+        this.container = builder.container;
+        this.payload = builder.payload;
+        this.clientSession = builder.clientSession;
     }
 
-    public void openSSHConnection(String username, String host, int port,
-                                  String password, String pathToKnownHostsFile) throws Exception {
-        conn.openSSHConnection(username, host, port, password, pathToKnownHostsFile);
-        channel = conn.getChannel();
+    public void setClientSession(Session clientSession) {
+        if (!this.clientSession.isOpen()) {
+            this.clientSession = clientSession;
+        }
     }
 
-    public void setSession(Session clientSession) {
-        this.clientSession = clientSession;
+    public void setChannel(ChannelShell channel) {
+        if (!channel.isConnected()) {
+            this.channel = channel;
+        }
     }
 
-    public void closeSSHConnection() throws IOException {
-        conn.closeSSHConnection();
-        channel = null;
+    public Profile getProfile() {
+        return new Profile(payload);
+    }
+
+    public Container getContainer() {
+        return container;
     }
 
     public ChannelShell getChannel() {
@@ -43,11 +50,38 @@ public class User {
         return clientSession;
     }
 
-    public UserInfo getUserInfo() {
-        return userInfo;
-    }
+    public static class UserBuilder {
+        private GoogleIdToken.Payload payload;
+        private Session clientSession;
+        private ChannelShell channel;
+        private Container container;
 
-    public String getUserInfoJsonString() {
-        return new Gson().toJson(userInfo);
+        public UserBuilder withIdToken(String idToken) {
+            try {
+                this.payload = Auth.getPayLoad(idToken);
+            } catch (IOException | GeneralSecurityException e) {
+                System.out.println(e);
+            }
+            return this;
+        }
+
+        public UserBuilder withSshChannel(ChannelShell channel) {
+            this.channel = channel;
+            return this;
+        }
+
+        public UserBuilder withContainer(Container container) {
+            this.container = container;
+            return this;
+        }
+
+        public UserBuilder withClientSession(Session session) {
+            this.clientSession = session;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
