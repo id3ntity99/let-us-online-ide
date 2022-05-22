fee35b95bc9057e287e952f64f68aa449de46f0e
id3ntity99 <jas06113@gmail.com>
2022-05-22 16:25:51 +0900

    feat: Interface for docker api

---
+++ src/main/java/com/letus/docker/ExecStartResultCallback.java
@@ -0,0 +1,42 @@
+package com.letus.docker;
+
+import com.github.dockerjava.api.async.ResultCallbackTemplate;
+import com.github.dockerjava.api.model.Frame;
+import com.letus.user.User;
+
+import javax.websocket.Session;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+public class ExecStartResultCallback extends ResultCallbackTemplate<ExecStartResultCallback, Frame> {
+    private Session session;
+
+    public ExecStartResultCallback(User user) {
+        this.session = user.getClientSession();
+    }
+
+    @Override
+    public void onNext(Frame item) {
+        if (item != null) {
+            try {
+                switch (item.getStreamType()) {
+                    case STDOUT:
+                    case RAW:
+                    case STDERR:
+                        if (session != null) {
+                            byte[] payload = item.getPayload();
+                            session.getBasicRemote().sendBinary(ByteBuffer.wrap(payload));
+                        }
+                        break;
+                    default:
+                        System.out.println("Unknown stream type: " + item.getStreamType());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Frame doesn't exist");
+        }
+    }
+}
