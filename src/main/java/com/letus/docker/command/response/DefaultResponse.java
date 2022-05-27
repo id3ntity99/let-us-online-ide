a001e67adda57e57481171814e89104b954c3a48
id3ntity99 <jas06113@gmail.com>
2022-05-27 18:19:49 +0900

    DefaultResponse deprecated

--- src/main/java/com/letus/docker/command/response/DefaultResponse.java
+++ src/main/java/com/letus/docker/command/response/DefaultResponse.java
@@ -1,5 +1,6 @@
 package com.letus.docker.command.response;
 
+@Deprecated
 public class DefaultResponse extends Response<Boolean>{
     public DefaultResponse(boolean isSuccess) {
         super(isSuccess);
