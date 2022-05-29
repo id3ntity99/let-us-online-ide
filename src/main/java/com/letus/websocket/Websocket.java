3d0797cba6a4b9da2e2b68c47ccb41a49c785c8c
id3ntity99 <jas06113@gmail.com>
2022-05-29 17:26:47 +0900

    ContainerCommands 대신 Commands 사용

--- src/main/java/com/letus/websocket/Websocket.java
+++ src/main/java/com/letus/websocket/Websocket.java
@@ -1,7 +1,9 @@
 package com.letus.websocket;
 
+import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Container;
-import com.letus.command.client.CommandFacade;
+import com.letus.docker.Client;
+import com.letus.docker.command.*;
 import com.letus.user.User;
 
 import javax.websocket.*;
@@ -13,42 +15,53 @@ import java.util.HashMap;
 
 @ServerEndpoint(value = "/init", configurator = HandShakeConfigurator.class)
 public class Websocket {
-    private static final String ID_TOKEN = "idToken";
-    private static final HashMap<String, User> sshChannelHashMap = new HashMap<>();
+    private final String ID_TOKEN = "idToken";
+    private final HashMap<String, User> tokenUserHash = new HashMap<>();
+    private final DockerClient dockerClient = Client.DOCKER_CLIENT.getDockerClient();
 
     @OnOpen
     public void onOpen(Session session) {
         String idToken = (String) session.getUserProperties().get(ID_TOKEN);
-        if (!sshChannelHashMap.containsKey(idToken)) {
+        if (!tokenUserHash.containsKey(idToken)) {
             session.setMaxIdleTimeout(1800000);
             try {
-                // TODO: 아래 코드를 감싸는 파사드 클래스 하나 만들기 (너무 지저분함).
-                // TODO: command.client.Commands를 파사드 클래스로 변경하여 Command Client로 사용.
-                Container container = CommandFacade.createContainerCmd("runner-image");
-                CommandFacade.startContainerCmd(container);
-                User user = new User.UserBuilder().withInputStream(new PipedInputStream())
+                User.UserBuilder userBuilder = new User.UserBuilder()
+                        .withInputStream(new PipedInputStream())
                         .withOutputStream(new PipedOutputStream())
-                        .withContainer(container)
-                        .withClientSession(session)
-                        .build();
-                String execId = CommandFacade.createExecContainerCmd(container);
-                CommandFacade.startExecContainerCmd(user, execId);
-                sshChannelHashMap.put(idToken, user);
+                        .withClientSession(session);
+                Container container = new CreateCmd().withImage("runner-image")
+                        .withDockerClient(dockerClient)
+                        .exec();
+                new StartCmd().withContainer(container)
+                        .withDockerClient(dockerClient)
+                        .exec();
+                User user = userBuilder.withContainer(container).build();
+                String execId = new CreateExecCmd().withContainer(container)
+                        .withDockerClient(dockerClient)
+                        .exec();
+                new StartExecCmd().withUser(user)
+                        .withExecId(execId)
+                        .withDockerClient(dockerClient)
+                        .exec();
+                tokenUserHash.put(idToken, user);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
             try {
-                User user = sshChannelHashMap.get(idToken);
+                User user = tokenUserHash.get(idToken);
                 user.setClientSession(session);
                 Container container = user.getContainer();
                 boolean isRunning = Boolean.TRUE.equals(
-                        CommandFacade.inspectContainer(container)
-                        .getInspection()
-                        .getState()
-                        .getRunning());
+                        new InspectCmd().withContainer(container)
+                                .withDockerClient(dockerClient)
+                                .exec()
+                                .getState()
+                                .getRunning());
                 if (!isRunning) {
-                    CommandFacade.startContainerCmd(container);
+                    new StartCmd().withDockerClient(dockerClient)
+                            .withContainer(container)
+                            .exec();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -60,7 +73,7 @@ public class Websocket {
     public void onMessage(Session session, String msg) {
         try {
             String idToken = (String) session.getUserProperties().get(ID_TOKEN);
-            User user = sshChannelHashMap.get(idToken);
+            User user = tokenUserHash.get(idToken);
             PipedOutputStream out = user.getOutputStream();
             out.write(msg.getBytes());
             out.flush();
@@ -72,7 +85,7 @@ public class Websocket {
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
         String idToken = (String) session.getUserProperties().get(ID_TOKEN);
-        User user = sshChannelHashMap.get(idToken);
+        User user = tokenUserHash.get(idToken);
         user.close();
     }
 }
