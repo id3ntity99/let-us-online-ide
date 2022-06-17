60878f63c5b47fa9305c24b2ce1a9e91a49f7119
id3ntity99 <jas06113@gmail.com>
2022-06-17 17:10:06 +0900

    Listener for sending request future

---
+++ NettyDockerClient/src/main/java/client/RequestFutureListener.java
@@ -0,0 +1,23 @@
+package client;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestFutureListener implements ChannelFutureListener {
+    private final Logger logger = LoggerFactory.getLogger(RequestFutureListener.class);
+    private final String uri;
+
+    public RequestFutureListener(String uri) {
+        this.uri = uri;
+    }
+
+    @Override
+    public void operationComplete(ChannelFuture future) throws Exception {
+        if (future.isSuccess() && future.isDone()) {
+            String loggerInfo = String.format("A request to %s is successfully done", uri);
+            logger.info(loggerInfo);
+        }
+    }
+}
