d457660528e8ea13453c3e220b3ef593b162fdcf
id3ntity99 <jas06113@gmail.com>
2022-06-20 16:51:06 +0900

    added javadoc

--- NettyDockerClient/src/main/java/client/nettyclient/initializers/HttpChannelInitializer.java
+++ NettyDockerClient/src/main/java/client/nettyclient/initializers/HttpChannelInitializer.java
@@ -1,14 +1,25 @@
-package client.nettyclient;
+package client.nettyclient.initializers;
 
+import client.nettyclient.handlers.HttpResponseHandler;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.DefaultMaxBytesRecvByteBufAllocator;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.HttpClientCodec;
 
+/**
+ * A {@link ChannelInitializer} for {@link io.netty.bootstrap.Bootstrap}.
+ * The {@link io.netty.bootstrap.Bootstrap} uses this initializer to create channel handlers.
+ */
 public class HttpChannelInitializer extends ChannelInitializer<NioSocketChannel> {
+    /**
+     * Init channel.
+     * This method created {@link DefaultMaxBytesRecvByteBufAllocator} to allocate {@link io.netty.buffer.ByteBuf} for receiving message.
+     * @param ch            the {@link io.netty.channel.Channel} which was registered.
+     */
     @Override
     public void initChannel(NioSocketChannel ch) {
-        //ch.config().setRecvByteBufAllocator(new DefaultMaxBytesRecvByteBufAllocator(80900, 80900));
+        DefaultMaxBytesRecvByteBufAllocator byteBufAlloc = new DefaultMaxBytesRecvByteBufAllocator(8092, 8092);
+        ch.config().setRecvByteBufAllocator(byteBufAlloc);
         ch.pipeline().addLast(new HttpClientCodec());
         ch.pipeline().addLast(new HttpResponseHandler());
     }
--- NettyDockerClient/src/main/java/client/nettyclient/listeners/RequestFutureListener.java
+++ NettyDockerClient/src/main/java/client/nettyclient/listeners/RequestFutureListener.java
@@ -1,26 +1,42 @@
-package client.nettyclient;
+package client.nettyclient.listeners;
 
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+
+/**
+ * An implementation of the {@link ChannelFutureListener}.
+ * This class instance is used for checking and logging that the
+ * request (using {@link io.netty.channel.Channel#writeAndFlush(Object)}) was successful.
+ * If the request was unsuccessful, this logs the cause.
+ */
 public class RequestFutureListener implements ChannelFutureListener {
     private final Logger logger = LoggerFactory.getLogger(RequestFutureListener.class);
     private final String uri;
 
-    public RequestFutureListener(String uri) {
-        this.uri = uri;
+    /**
+     * A constructor that receives {@link URI} parameter and convert to string for logging.
+     * @param uri A {@link URI} for logging.
+     */
+    public RequestFutureListener(URI uri) {
+        this.uri = uri.toString();
     }
 
+
+    /**
+     * Invoked when the operation was completed.
+     * @param future  the source {@link ChannelFuture} which called this callback
+     */
     @Override
-    public void operationComplete(ChannelFuture future) throws Exception {
+    public void operationComplete(ChannelFuture future) {
         if (future.isSuccess() && future.isDone()) {
             String loggerInfo = String.format("A request to %s is successfully done", uri);
             logger.info(loggerInfo);
         } else {
             logger.debug("Exception raised while writing...", future.cause());
-            future.cause().printStackTrace();
         }
     }
 }
