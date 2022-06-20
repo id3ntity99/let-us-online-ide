2e57b0438976a14261bf59182464909ecd3781cb
id3ntity99 <jas06113@gmail.com>
2022-06-20 16:02:28 +0900

    Added javadocs

--- NettyDockerClient/src/main/java/client/docker/commands/CreateContainerResponse.java
+++ NettyDockerClient/src/main/java/client/docker/commands/CreateContainerResponse.java
@@ -1,7 +1,21 @@
 package client.docker.commands;
 
+import client.nettyclient.response.SimpleResponse;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.netty.handler.codec.http.FullHttpRequest;
 
+import java.net.URI;
+
+/**
+ * This class is used to contain HTTP response body.
+ * You can use this object to map JSON.
+ * The fields are extracted from {@link SimpleResponse#getBody()}.
+ * Thus, the overall flow is: <br/>
+ * <br/>
+ * {@link client.nettyclient.HttpClient#request(URI, FullHttpRequest)} returns SimpleResponse <br/>
+ * -> {@link CreateContainerCmd#exec()} checks if the container is created normally and <br/>
+ * -> map the {@link SimpleResponse#getBody()} (which returns JSON string) to {@link CreateContainerResponse}
+ */
 public class CreateContainerResponse {
     @JsonProperty("Id")
     private String id;
