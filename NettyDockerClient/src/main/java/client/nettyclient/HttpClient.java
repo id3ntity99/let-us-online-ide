ee0867c2ecc8aea79135918c0bb293f9070fab15
id3ntity99 <jas06113@gmail.com>
2022-06-20 16:26:35 +0900

    Added javadocs and changed logic

--- NettyDockerClient/src/main/java/client/nettyclient/HttpClient.java
+++ NettyDockerClient/src/main/java/client/nettyclient/HttpClient.java
@@ -1,13 +1,16 @@
 package client.nettyclient;
 
+import client.nettyclient.handlers.HttpResponseHandler;
+import client.nettyclient.initializers.HttpChannelInitializer;
+import client.nettyclient.listeners.RequestFutureListener;
+import client.nettyclient.response.SimpleResponse;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.util.concurrent.DefaultPromise;
-import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.Promise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +19,6 @@ import java.net.URI;
 import java.util.concurrent.ExecutionException;
 
 public class HttpClient {
-    private ChannelFuture channelFuture;
     private final Logger logger = LoggerFactory.getLogger(HttpClient.class);
 
     /**
@@ -25,58 +27,52 @@ public class HttpClient {
      * each channel when the channel has been connected.
      * Each {@link io.netty.channel.EventLoop} contains {@link io.netty.channel.ChannelPipeline}, which contains one or more
      * {@link io.netty.channel.ChannelHandler}.
-     * If there is any events to process in the channels, the EventLoops catch them and pass to the ChannelPipelines to process them as required.
+     * If there is any events to process in the channels, the EventLoops catch and pass them to the ChannelPipelines to process them as required.
+     * Note that, the connection process will be blocking.
      *
      * @param uri Target URI to connect.
      * @throws InterruptedException will be thrown when the connection process has been interrupted before completion.
      */
-    public void connect(URI uri) throws InterruptedException {
+    private Channel connect(URI uri) throws InterruptedException {
         EventLoopGroup group = new NioEventLoopGroup();
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.channel(NioSocketChannel.class)
                 .group(group)
                 .remoteAddress(uri.getHost(), uri.getPort())
                 .handler(new HttpChannelInitializer());
-        channelFuture = bootstrap.connect().sync();
+        ChannelFuture channelFuture = bootstrap.connect().sync();
         if (channelFuture.isSuccess() && channelFuture.isDone()) {
             String loggerInfo = String.format("HTTP connection to %s is established", uri);
             logger.info(loggerInfo);
         }
+        return channelFuture.channel();
     }
 
-    /**
-     * Create {@link Promise<SimpleResponse>} for later use.
-     * The promise is used to get and return the response.
-     *
-     * @return {@link Promise<SimpleResponse>}.
-     */
-    private Promise<SimpleResponse> createPromise() {
-        EventExecutor executor = channelFuture.channel()
-                .pipeline()
-                .context(HttpResponseHandler.class)
-                .executor();
-        // Create new promise for later use.
-        return new DefaultPromise<>(executor);
-    }
 
     /**
-     * Initiate the HTTP request. If {@link io.netty.channel.ChannelInboundHandler} receives the response, it will write
-     * the response to the {@link Promise}.
+     * Initiate the asynchronous HTTP request.
+     * If this method is invoked, it will automatically connect to the remote address, create the {@link EventLoopGroup},
+     * establish the connection(the channel).
+     * After sending the request, the {@link HttpResponseHandler} will receive the Http response, and set the promise to success.
+     * As soon as the promise has been set to success, its result will be returned.
+     * Even if this method makes use of asynchronous request, it will block until the promise is marked as success with the result.
      *
-     * @param targetURL A URI to send request.
+     * @param uri A URI to send request.
      * @param req       A {@link FullHttpRequest} to be sent.
      * @return A {@link SimpleResponse} extracted from the {@link Promise}.
      * @throws InterruptedException occurs when sending request has been interrupted.
      * @throws ExecutionException   occurs when {@link Promise#get()}.
      */
-    public SimpleResponse request(URI targetURL, FullHttpRequest req) throws InterruptedException, ExecutionException {
-        Promise<SimpleResponse> promise = createPromise();
-        // Set promise to the handler.
-        channelFuture.channel().pipeline().get(HttpResponseHandler.class).setPromise(promise);
-        // Send request.
-        channelFuture.channel()
-                .writeAndFlush(req)
-                .addListener(new RequestFutureListener(targetURL.toString()));
+    public SimpleResponse request(URI uri, FullHttpRequest req) throws InterruptedException, ExecutionException {
+        Channel channel = connect(uri);
+        Promise<SimpleResponse> promise = channel.eventLoop().newPromise();
+        channel.pipeline()
+                .get(HttpResponseHandler.class)
+                .setPromise(promise);
+        channel.writeAndFlush(req).addListener(new RequestFutureListener(uri));
         return promise.get();
     }
+
+    // TODO
+    //  public void attach();
 }
