1cb4517b48e4285dd77f2ccb52922378fce922c0
id3ntity99 <jas06113@gmail.com>
2022-06-21 12:34:30 +0900

    Added the inner class for request

--- NettyDockerClient/src/main/java/client/docker/commands/CreateContainerCmd.java
+++ NettyDockerClient/src/main/java/client/docker/commands/CreateContainerCmd.java
@@ -10,12 +10,14 @@ import client.json.configs.hostconfig.HostConfig;
 import client.json.configs.hostconfig.networkingconfig.NetworkingConfig;
 import client.nettyclient.HttpClient;
 import client.nettyclient.response.SimpleResponse;
-import client.request.POSTRequest;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
 
 import java.net.URI;
 import java.util.Map;
@@ -26,12 +28,11 @@ import java.util.Map;
  * All the user has to do is just using the methods of this class to configure the {@link Config},
  * and invoke {@link CreateContainerCmd#exec()} to make a request.
  */
-public class CreateContainerCmd extends AbstractCommand<CreateContainerResponse>{
+public class CreateContainerCmd extends AbstractCommand<CreateContainerResponse> {
     private final HttpClient httpClient = new HttpClient();
     private final Config config = new Config();
     private final ObjectMapper mapper = new ObjectMapper().setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
     private final ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
-    private final URI targetUri = URIs.CREATE_CONTAINER.uri();
 
     public CreateContainerCmd withHostConfig(HostConfig hostConfig) {
         config.setHostConfig(hostConfig);
@@ -168,12 +169,12 @@ public class CreateContainerCmd extends AbstractCommand<CreateContainerResponse>
         return this;
     }
 
+    @Override
     public CreateContainerResponse exec() throws Exception {
-        String jsonString = writer.writeValueAsString(config);
-        FullHttpRequest req = new POSTRequest().withURI(targetUri)
-                .withBody(jsonString)
-                .build();
-        SimpleResponse simpleRes = httpClient.request(URIs.CREATE_CONTAINER.uri(), req);
+        String jsonBody = writer.writeValueAsString(config);
+        URI uri = new URI(URIs.CREATE_CONTAINER.uri());
+        FullHttpRequest req = CreateContainerRequest.create(uri, jsonBody);
+        SimpleResponse simpleRes = httpClient.request(uri, req);
         if (simpleRes.getStatusCode() != 201) {
             String errMsg = String.format(
                     "[%s] Cannot create container due to: %s",
@@ -185,5 +186,20 @@ public class CreateContainerCmd extends AbstractCommand<CreateContainerResponse>
         String info = String.format("Container created: %s", simpleRes.getBody());
         logger.info(info);
         return mapper.readValue(simpleRes.getBody(), CreateContainerResponse.class);
+    }
+
+    private static class CreateContainerRequest {
+        public static FullHttpRequest create(URI uri, String body) {
+            FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, uri.getRawPath());
+            ByteBuf bodyBuffer = Unpooled.copiedBuffer(body, CharsetUtil.UTF_8);
+            req.headers().set(HttpHeaderNames.HOST, uri.getHost());
+            req.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
+            req.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
+            req.headers().set(HttpHeaderNames.TRANSFER_ENCODING, HttpHeaderValues.CHUNKED);
+            req.content().writeBytes(bodyBuffer);
+            req.headers().add(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON);
+            req.headers().set(HttpHeaderNames.CONTENT_LENGTH, bodyBuffer.readableBytes());
+            return req;
+        }
     }
 }
