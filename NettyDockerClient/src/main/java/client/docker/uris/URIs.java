b866ece04c8b12cd5d6dcd6b33242050e822bc84
id3ntity99 <jas06113@gmail.com>
2022-06-21 12:34:56 +0900

    uri() returns string uri

--- NettyDockerClient/src/main/java/client/docker/uris/URIs.java
+++ NettyDockerClient/src/main/java/client/docker/uris/URIs.java
@@ -3,25 +3,19 @@ package client.docker.uris;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public enum URIs {
     DOCKER_DAEMON("http://localhost:2375"),
-    CREATE_CONTAINER("http://localhost:2375/containers/create");
+    CREATE_CONTAINER("http://localhost:2375/containers/create"),
+    START_CONTAINER("http://localhost:2375/containers/%s/start");
 
-    private URI uri;
+    private String uri;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    URIs(String uriString) {
-        try {
-            this.uri = new URI(uriString);
-        } catch(URISyntaxException e) {
-           logger.error("Something's wrong about uri...", e);
-        }
+    URIs(String stringUri) {
+        this.uri = stringUri;
     }
 
-    public URI uri() {
+    public String uri() {
         return uri;
     }
 }
