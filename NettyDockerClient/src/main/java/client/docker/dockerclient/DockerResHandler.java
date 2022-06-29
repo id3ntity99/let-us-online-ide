2bc92c56a2814d315cd6bc0e28915b06aadb9e73
id3ntity99 <jas06113@gmail.com>
2022-06-29 15:30:04 +0900

    docker client and handlers

---
+++ NettyDockerClient/src/main/java/client/docker/dockerclient/DockerResHandler.java
@@ -0,0 +1,29 @@
+package client.docker.dockerclient;
+
+import client.nettyserver.SimpleResponse;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.Promise;
+
+public class DockerResHandler extends SimpleChannelInboundHandler<FullHttpResponse> {
+    private Promise<SimpleResponse> promise;
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse res) throws Exception {
+        if (res.status().code() >= 200 && res.status().code() <= 299) {
+            SimpleResponse simpleResponse = new SimpleResponse();
+            simpleResponse.setStatusCode(res.status().code());
+            simpleResponse.setBody(res.content().toString(CharsetUtil.UTF_8));
+            promise.setSuccess(simpleResponse);
+        } else {
+            // TODO Create specific Exception for this case.
+            throw new Exception("Request failed");
+        }
+    }
+
+    public void setPromise(Promise<SimpleResponse> promise) {
+        this.promise = promise;
+    }
+}
