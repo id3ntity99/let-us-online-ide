ea6ee0d8a6897881193e8cf18c99b21c6f3d12ea
id3ntity99 <jas06113@gmail.com>
2022-07-03 16:52:31 +0900

    Interface for docker client

---
+++ NettyDockerClient/src/main/java/client/docker/dockerclient/proxy/DockerClient.java
@@ -0,0 +1,13 @@
+package client.docker.dockerclient.proxy;
+
+import client.nettyserver.SimpleResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.concurrent.Promise;
+
+public interface DockerClient {
+    DockerClient bootstrap();
+    ChannelFuture connect();
+    Promise<SimpleResponse> request(FullHttpRequest req);
+    ChannelFuture execute(FullHttpRequest req);
+}
