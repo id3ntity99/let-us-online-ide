ed9e6970067d5f07565ac748a4e726d90943ef2c
id3ntity99 <jas06113@gmail.com>
2022-07-03 20:54:22 +0900

    added uri(String id) method

--- NettyDockerClient/src/main/java/client/docker/uris/URIs.java
+++ NettyDockerClient/src/main/java/client/docker/uris/URIs.java
@@ -6,16 +6,24 @@ import org.slf4j.LoggerFactory;
 public enum URIs {
     DOCKER_DAEMON("http://localhost:2375"),
     CREATE_CONTAINER("http://localhost:2375/containers/create"),
-    START_CONTAINER("http://localhost:2375/containers/%s/start");
+    START_CONTAINER("http://localhost:2375/containers/%s/start"),
+    EXEC_CREATE("http://localhost:2375/containers/%s/exec"),
+    EXEC_START("http://localhost:2375/exec/%s/start"),
+    KILL_CONTAINER("http://localhost:2375/containers/%s/kill");
 
-    private String uri;
+    private String stringUri;
+    private String id;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     URIs(String stringUri) {
-        this.uri = stringUri;
+        this.stringUri = stringUri;
     }
 
     public String uri() {
-        return uri;
+        return stringUri;
+    }
+
+    public String uri(String id) {
+        return String.format(stringUri, id);
     }
 }
